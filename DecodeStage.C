f94c1702a0825213337a04a6dc95a14fcc5b03c4
Nischinth Murari <murarins@appstate.edu>
2019-10-27 13:22:13 -0400

    Finished implementing DecodeStage.C for lab 8

--- DecodeStage.C
+++ DecodeStage.C
@@ -12,7 +12,7 @@
 #include "DecodeStage.h"
 #include "Status.h"
 #include "Debug.h"
-
+#include "Instructions.h"
 
 /*
  * doClockLow:
@@ -25,20 +25,132 @@
  */
 bool DecodeStage::doClockLow(PipeReg ** pregs, Stage ** stages)
 {
-   D * dreg = (D *) pregs[DREG];
-   E * ereg = (E *) pregs[EREG];
+    D * dreg = (D *) pregs[DREG];
+    E * ereg = (E *) pregs[EREG];
+
+    uint64_t icode = 0, ifun = 0, valC = 0, valA = 0, valB = 0;
+    uint64_t dstE = RNONE, dstM = RNONE, srcA = RNONE, srcB = RNONE, stat = SAOK; 
+
+    icode = dreg-> geticode()-> getOutput();
+    ifun = dreg-> getifun()-> getOutput();
+    valC = dreg-> getvalC()-> getOutput(); 
+    stat = dreg-> getstat() -> getOutput();
    
-   uint64_t icode = 0, ifun = 0, valC = 0;
-   uint64_t dstE = RNONE, dstM = RNONE, srcA = RNONE, srcB = RNONE, stat = SAOK; 
-   
-   icode = dreg-> geticode()-> getOutput();
-   ifun = dreg-> getifun()-> getOutput();
-   valC = dreg-> getvalC()-> getOutput(); 
-   stat = dreg-> getstat() -> getOutput();
-   
-   setEInput(ereg,stat, icode,ifun, valC, 0, 0, dstE, dstM, srcA, srcB);
-   
-   return false;
+    valA = sel_FwdA(dreg);
+    valB = FwdB(dreg);
+    dstE = dst_E(dreg, icode);
+    dstM = dst_M(dreg, icode);
+    srcA = d_srcA(dreg, icode);
+    srcB = d_srcB(dreg, icode);
+
+    setEInput(ereg,stat, icode,ifun, valC, valA, valB, dstE, dstM, srcA, srcB);
+
+    return false;
+}
+
+
+
+/* Simulation of HCL for d_srcA
+ * @param: D* - A pointer to an instance of a Decode pipe register class.
+ * @param: icode - The icode of the instruction passing through the pipeline.
+ * @return: The appropriate d_srcA value given the icode.
+ */
+uint8_t DecodeStage::d_srcA(D * dreg, uint8_t icode)
+{
+    uint8_t D_rA_ret[4] = {IRRMOVQ, IRMMOVQ, IOPQ, IPUSHQ};
+    uint8_t RSP_ret[2] = {IPOPQ, IRET};
+    for(uint8_t code : D_rA_ret)
+    {
+        if(code == icode) return dreg->getrA()->getOutput();
+    }
+    for(uint8_t code : RSP_ret)
+    {
+        if(code == icode) return RSP;
+    }
+    return RNONE;
+}
+
+/* Simulation of HCL for d_srcB
+ * @param: D* - A pointer to an instance of a Decode pipe register class.
+ * @param: icode - The icode of the instruction passing through the pipeline.
+ * @return: The appropriate d_srcB value given the icode.
+ */
+uint8_t DecodeStage::d_srcB(D * dreg, uint8_t icode)
+{
+    uint8_t D_rB_ret[4] = {IOPQ, IRMMOVQ, IMRMOVQ};
+    uint8_t RSP_ret[4] = {IPUSHQ, IPOPQ, ICALL, IRET};
+    for(uint8_t code : D_rB_ret)
+    {
+        if(code == icode) return dreg->getrB()->getOutput();
+    }
+    for(uint8_t code : RSP_ret)
+    {
+        if(code == icode) return RSP;
+    }
+    return RNONE;
+}
+
+
+/* Simulation of HCL for dst_E
+ * @param: D* - A pointer to an instance of a Decode pipe register class.
+ * @param: icode - The icode of the instruction passing through the pipeline.
+ * @return: The appropriate dst_E value given the icode.
+ */
+uint8_t DecodeStage::dst_E(D * dreg, uint8_t icode)
+{
+    uint8_t D_rB_ret[3] = {IRRMOVQ, IIRMOVQ, IOPQ};
+    uint8_t RSP_ret[4] = {IPUSHQ, IPOPQ, ICALL, IRET};
+    for(uint8_t code : D_rB_ret)
+    {
+        if(code == icode) return dreg->getrB()->getOutput();
+    }
+    for(uint8_t code : RSP_ret)
+    {
+        if(code == icode) return RSP;
+    }
+    return RNONE;
+}
+
+/* Simulation of HCL for dst_M
+ * @param: D* - A pointer to an instance of a Decode pipe register class.
+ * @param: icode - The icode of the instruction passing through the pipeline.
+ * @return: The appropriate dst_M value given the icode.
+ */
+uint8_t DecodeStage::dst_M(D * dreg, uint8_t icode)
+{
+    uint8_t D_rA_ret[2] = {IMRMOVQ, IPOPQ};
+    for(uint8_t code : D_rA_ret)
+    {
+        if(code == icode) return dreg->getrA()->getOutput();
+    }
+    return RNONE;
+}
+
+/**
+ * Simplified Sel + FwdA HCL.
+ * @param: dreg -  A pointer to an instance of the D pipe register class.
+ * @return: d_rvalA 
+ */
+uint64_t DecodeStage::sel_FwdA(D* dreg)
+{
+    uint8_t rA =  dreg->getrA()->getOutput();
+    bool error = false;
+    RegisterFile * regFile = RegisterFile::getInstance();
+    return regFile->readRegister(rA, error);
+}
+
+
+/**
+ * Simplified FwdB 
+ * @param: derg - A pointer to an instance of the D Pipe register class.
+ * @return: d_rvalB
+ */
+uint64_t DecodeStage::FwdB(D* dreg)
+{
+    uint8_t rB =  dreg->getrB()->getOutput();
+    bool error = false;
+    RegisterFile * regFile = RegisterFile::getInstance();
+    return regFile->readRegister(rB, error);
 }
 
 /* doClockHigh
@@ -49,27 +161,27 @@ bool DecodeStage::doClockLow(PipeReg ** pregs, Stage ** stages)
  */
 void DecodeStage::doClockHigh(PipeReg ** pregs)
 {
-   D * dreg = (D *) pregs[DREG];
-   E * ereg = (E *) pregs[EREG];
+    D * dreg = (D *) pregs[DREG];
+    E * ereg = (E *) pregs[EREG];
 
-   ereg->getstat()->normal();
-   ereg->geticode()->normal();
-   ereg->getifun()->normal();
-   ereg->getvalC()->normal();
-   ereg->getvalA()->normal();
-   ereg->getvalB()->normal(); 
-   ereg->getdstE()->normal();
-   ereg->getdstM()->normal();
-   ereg->getsrcA()->normal();
-   ereg->getsrcB()->normal();
+    ereg->getstat()->normal();
+    ereg->geticode()->normal();
+    ereg->getifun()->normal();
+    ereg->getvalC()->normal();
+    ereg->getvalA()->normal();
+    ereg->getvalB()->normal(); 
+    ereg->getdstE()->normal();
+    ereg->getdstM()->normal();
+    ereg->getsrcA()->normal();
+    ereg->getsrcB()->normal();
 
-   dreg->getstat()->normal();
-   dreg->geticode()->normal();
-   dreg->getifun()->normal();
-   dreg->getrA()->normal();
-   dreg->getrB()->normal();
-   dreg->getvalC()->normal();
-   dreg->getvalP()->normal();
+    dreg->getstat()->normal();
+    dreg->geticode()->normal();
+    dreg->getifun()->normal();
+    dreg->getrA()->normal();
+    dreg->getrB()->normal();
+    dreg->getvalC()->normal();
+    dreg->getvalP()->normal();
 }
 
 /* setDInput
@@ -84,18 +196,18 @@ void DecodeStage::doClockHigh(PipeReg ** pregs)
  * @param: rB - value to be stored in the rB pipeline register within D
  * @param: valC - value to be stored in the valC pipeline register within D
  * @param: valP - value to be stored in the valP pipeline register within D
-*/
+ */
 void DecodeStage::setEInput(E * ereg, uint64_t stat, uint64_t icode, 
-                           uint64_t ifun, uint64_t valC, uint64_t valA,
-                           uint64_t valB, uint64_t dstE, uint64_t dstM, uint64_t srcA, uint64_t srcB)
+        uint64_t ifun, uint64_t valC, uint64_t valA,
+        uint64_t valB, uint64_t dstE, uint64_t dstM, uint64_t srcA, uint64_t srcB)
 {
-   ereg->getstat()->setInput(stat);
-   ereg->geticode()->setInput(icode);
-   ereg->getifun()->setInput(ifun);
-   ereg->getvalC()->setInput(valC);
-   ereg->getdstE()->setInput(dstE);
-   ereg->getdstM()->setInput(dstM);
-   ereg->getsrcA()-> setInput(srcA);
-   ereg->getsrcB()->setInput(srcB);
+    ereg->getstat()->setInput(stat);
+    ereg->geticode()->setInput(icode);
+    ereg->getifun()->setInput(ifun);
+    ereg->getvalC()->setInput(valC);
+    ereg->getdstE()->setInput(dstE);
+    ereg->getdstM()->setInput(dstM);
+    ereg->getsrcA()-> setInput(srcA);
+    ereg->getsrcB()->setInput(srcB);
 }
-     
+
--- DecodeStage.h
+++ DecodeStage.h
@@ -6,6 +6,13 @@ class DecodeStage: public Stage
       void setEInput(E * dreg, uint64_t stat, uint64_t icode, uint64_t ifun, 
                      uint64_t valC, uint64_t valA, uint64_t valB, uint64_t dstE,
                      uint64_t dstM, uint64_t srcA, uint64_t srcB);
+      uint8_t d_srcA(D * dreg, uint8_t icode);
+      uint8_t d_srcB(D * dreg, uint8_t icode);
+      uint8_t dst_E(D * dreg, uint8_t icode);
+      uint8_t dst_M(D * dreg, uint8_t icode);
+      uint64_t sel_FwdA(D* dreg);
+      uint64_t FwdB(D* dreg);
+   
    public:
       bool doClockLow(PipeReg ** pregs, Stage ** stages);
       void doClockHigh(PipeReg ** pregs);
