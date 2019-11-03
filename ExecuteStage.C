0fffa0abf8de9f055c817401bbccb0625b10356e
Nischinth Murari <murarins@appstate.edu>
2019-11-03 17:42:00 -0500

    22/27 Tests pass. Parts 3 and 4 remain

--- ExecuteStage.C
+++ ExecuteStage.C
@@ -12,6 +12,12 @@
 #include "ExecuteStage.h"
 #include "Status.h"
 #include "Debug.h"
+#include "Instructions.h"
+#include "ConditionCodes.h"
+#include "Tools.h"
+
+void clearCC(ConditionCodes* codes);
+bool Cnd = false;
 
 /*
  * doClockLow:
@@ -26,15 +32,21 @@ bool ExecuteStage::doClockLow(PipeReg ** pregs, Stage ** stages)
 {
     E * ereg = (E *) pregs[EREG];
     M * mreg = (M *) pregs[MREG];
-    
 
-    uint64_t stat = SAOK, icode = 0, Cnd = 0, valE = 0, valA = 0, dstM = RNONE, dstE = RNONE;
+
+    uint64_t stat = SAOK, icode = 0, valE = 0, valA = 0, dstM = RNONE, dstE = RNONE;
     stat = ereg->getstat()->getOutput();
     icode = ereg->geticode()->getOutput();
     dstM = ereg->getdstM()->getOutput();
-    dstE = ereg->getdstE()->getOutput();
-    valE = ereg->getvalC()->getOutput();
+    dstE = e_dstE(ereg);
+    valE = ALU(ereg); 
 
+    if(set_cc(icode))
+    {
+        CC(ereg, valE);
+    }
+
+    valA = ereg->getvalA()->getOutput();
     setMInput(mreg, stat, icode, Cnd, valE, valA, dstE, dstM);
     return 0;
 }
@@ -67,7 +79,7 @@ void ExecuteStage::doClockHigh(PipeReg ** pregs)
     mreg->getvalA()->normal();
     mreg->getdstE()->normal();
     mreg->getdstM()->normal(); 
-    
+
 }
 
 void ExecuteStage::setMInput(M* mreg, uint64_t stat, uint64_t icode, uint64_t Cnd, uint64_t valE, uint64_t valA, uint64_t dstE, uint64_t dstM)
@@ -80,3 +92,138 @@ void ExecuteStage::setMInput(M* mreg, uint64_t stat, uint64_t icode, uint64_t Cn
     mreg->getdstE()->setInput(dstE);
     mreg->getdstM()->setInput(dstM);
 }
+
+/**
+ *Simulator for aluA HCL 
+ *@param: A pointer to the instance of the E register class.
+ *@return: Appropriate aluA value depending on E_icode
+ */
+int64_t ExecuteStage::aluA(E * ereg)
+{
+    uint8_t E_icode = ereg->geticode()->getOutput();
+    if(E_icode == IRRMOVQ || E_icode == IOPQ) return ereg->getvalA()->getOutput();
+    if(E_icode == IIRMOVQ || E_icode == IRMMOVQ || E_icode == IMRMOVQ) return ereg->getvalC()->getOutput();
+    if(E_icode == ICALL || E_icode == IPUSHQ) return -8;
+    if(E_icode == IRET || E_icode == IPOPQ) return -8; 
+    return 0;
+}
+
+/*
+ * Simulator for aluB HCL
+ * @param: A pointer to the instance of the E register class.
+ * @return: Appropriate aluB value depending on E_icode
+ */
+int64_t ExecuteStage::aluB(E* ereg)
+{
+    uint8_t E_icode = ereg->geticode()->getOutput();
+    if(E_icode == IRRMOVQ || E_icode == IIRMOVQ) return 0;
+    uint8_t E_valB[] = {IRMMOVQ, IMRMOVQ, IOPQ, ICALL, IPUSHQ, IRET, IPOPQ};
+    for(uint8_t icode : E_valB)
+        if(E_icode == icode) return ereg->getvalB()->getOutput();
+    return 0;
+}
+
+/*
+ * Simulator for aluFUN HCL
+ * @param: A pointer to the instance of the E register class.
+ * @return: Appropriate aluFUN value depending on E_icode
+ */
+uint8_t ExecuteStage::aluFUN(E* ereg)
+{
+    uint8_t E_icode = ereg->geticode()->getOutput();
+    if(E_icode == IOPQ) return ereg->getifun()->getOutput();
+    return 0;
+}
+
+/**
+ * Simulator for set_cc HCL
+ * @param: A pointer to the instance of the E register class.
+ * @return: True if E_icode == IOPQ
+ */
+bool ExecuteStage::set_cc(uint8_t icode)
+{
+    return (icode == IOPQ);
+}
+
+/**
+ * Simulator for e_dstE HCL 
+ * @param: pregs - Array of pipeline registers
+ *
+ * @param: A pointer to the instance of the E register class.
+ * @return: The appropriate e_dstE value depending on E_icode
+ */
+int64_t ExecuteStage::e_dstE(E* ereg)
+{    
+    if(ereg->geticode()->getOutput() == IRRMOVQ && !Cnd) return RNONE;
+    return ereg->getdstE()->getOutput();
+}
+
+/**
+ * Simulator for CC unit
+ * Sets the condition codes if set_cc returns true
+ * @param: A pointer to the instance of the E register class.
+ */
+void ExecuteStage::CC(E* ereg, int64_t valE)
+{
+    ConditionCodes * codes = ConditionCodes::getInstance();
+    clearCC(codes);
+    uint8_t ifun = aluFUN(ereg);
+    bool error = false;
+    if(valE == 0) //if valE is 0, then set the ZF code and return.
+    {
+        codes->setConditionCode(1,ZF,error); 
+    }
+    if(ifun == 0) //if ifun is ADDQ, then check if there was overflow and set OF code.
+    {
+        if(Tools::addOverflow(aluA(ereg), aluB(ereg)))
+        {
+            codes->setConditionCode(1,OF,error); 
+        }
+    }
+    else if(ifun == 1)//or if ifun is SUBQ, then check if there was overflow and set OF code.
+    {
+        if(Tools::subOverflow(aluA(ereg), aluB(ereg)))
+        {
+            codes->setConditionCode(1,OF,error);
+        }
+    }
+
+    if(Tools::sign(valE) == 1)  //check if valE is negative and set SF if so.
+    {
+        codes->setConditionCode(1,SF,error); 
+        return;
+    }
+}
+/**
+ * Simulator of the ALU computational unit.
+ * @param: ereg - A pointer to the instance of the E register class.
+ * @return: valE - a 64 bit value 
+ */
+
+int64_t ExecuteStage::ALU(E* ereg)
+{
+    uint64_t A = aluA(ereg);
+    uint64_t B = aluB(ereg);
+    uint8_t ifun = aluFUN(ereg);
+    switch(ifun)
+    {
+        case 1 : return A - B; //SUBQ
+        case 2:  return A & B; //ANDQ
+        case 3:  return A ^ B; //XORQ 
+        default: break;
+    }
+    return A + B; //ANDQ
+}
+
+/**
+ * Helper method that clears the condition codes before each call to CC. Precautionary.
+ * @param: codes - A pointer to the ConditionCodes instance
+ */
+void clearCC(ConditionCodes* codes)
+{
+    bool error = false;
+    codes->setConditionCode(0,OF,error);
+    codes->setConditionCode(0,ZF,error);
+    codes->setConditionCode(0,SF,error);
+}
+
--- ExecuteStage.h
+++ ExecuteStage.h
@@ -4,8 +4,14 @@ class ExecuteStage: public Stage
 {
     private:
         void setMInput(M* mreg, uint64_t stat, uint64_t icode, uint64_t Cnd, uint64_t valE, uint64_t valA, uint64_t dstE, uint64_t dstM);
-
+        int64_t aluA(E* ereg);
+        int64_t aluB(E* ereg);
+        uint8_t aluFUN(E* ereg);    
+        bool set_cc(uint8_t icode);
+        int64_t e_dstE(E* ereg);
+        int64_t ALU(E* ereg);
+        void CC(E* ereg, int64_t valE);
     public:
-        bool doClockLow(PipeReg ** pregs, Stage ** stages);
-        void doClockHigh(PipeReg ** pregs);
+            bool doClockLow(PipeReg ** pregs, Stage ** stages);
+            void doClockHigh(PipeReg ** pregs);
 };
